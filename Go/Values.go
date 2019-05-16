3947d5b2a242d440d40cf2d7769159b641b3d5ba
Mauricio Sevilla <jmsevillam@unal.edu.co>
2019-05-16 11:19:47 -0500

    First Go program added

---
+++ Go/Values.go
@@ -0,0 +1,21 @@
+/*
+This code is done to understand how some of the variables can be used
+Look that it seems a combination between C++ and Python
+*/
+package main
+import "fmt"
+
+func main(){
+
+	fmt.Println("Go"+"Language"+"Learning")
+	fmt.Println("1+1",1+1)
+	fmt.Println("1*2",1*2)
+	fmt.Println("1/2",1/2)
+	fmt.Println("1/2.",1/2.)
+	fmt.Println("1./2",1./2)
+	fmt.Println(true && false)
+	fmt.Println(true || false)
+	fmt.Println(!true)
+
+
+}
---
+++ Go/hello.go
@@ -0,0 +1,13 @@
+/*
+This is the most basic program on Go.
+
+It prints "Hello Word" to the prompt.
+*/
+package main
+
+import "fmt"
+
+func main(){
+fmt.Printf("Hello World \n")
+
+}
