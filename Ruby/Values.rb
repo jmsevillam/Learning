2bde77530af6cacc90a4bd15f3bc3c637f134217
Mauricio Sevilla <jmsevillam@unal.edu.co>
2019-05-16 12:51:42 -0500

    Ruby vals added

---
+++ Ruby/Values.rb
@@ -0,0 +1,14 @@
+print "1+1 ",1+1
+puts
+print "1*2 ",1*2
+puts
+print "1/2 ", 1/2
+puts
+print "1/2.0 ", 1/2.0, " Must include .0"
+puts
+print "1.0/2 ", 1.0/2, " Must include .0"
+puts
+print true and false
+puts
+print true or false
+puts
