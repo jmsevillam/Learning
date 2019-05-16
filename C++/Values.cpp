1668cf79f3ddbeaa2b4542964eb718b1368346f0
Mauricio Sevilla <jmsevillam@unal.edu.co>
2019-05-16 11:30:48 -0500

    First C++ code added

---
+++ C++/Values.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main(){
+
+
+std::cout<<"1+1 "<<1+1<<std::endl;
+std::cout<<"1*2 "<<1+1<<std::endl;
+std::cout<<"1/2 "<<1+1<<std::endl;
+std::cout<<"1/2. "<<1+1<<std::endl;
+std::cout<<"1./2 "<<1+1<<std::endl;
+std::cout<<(true && false)<<std::endl;
+std::cout<<(true || false)<<std::endl;
+std::cout<<! true<<std::endl;
+return 0;
+}
