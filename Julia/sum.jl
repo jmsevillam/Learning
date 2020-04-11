9da9bf8dce536cbaf9438fff78cc4e48811f3335
Mauricio Sevilla <jmsevillam@unal.edu.co>
2020-04-11 15:50:12 +0200

    New example added

---
+++ Julia/sum.jl
@@ -0,0 +1,9 @@
+function f(a,b)
+  return a+b
+end
+
+A=f(1.,2)
+b=f(1.,2.)
+C=f(1,2)
+
+println(A," ",b,"  ",C)
---
+++ Julia/sum.py
@@ -0,0 +1,9 @@
+def f(a,b):
+    return a+b
+
+A=f(1.,2)
+b=f(1.,2.)
+C=f(1,2)
+
+print(A,b,C)
+
