1f066705b004130dde6794d2a1b308a5eaa9cb58
Mauricio Sevilla <jmsevillam@unal.edu.co>
2020-04-19 19:33:16 +0200

    random initial velocity

--- Javascript/box/box.js
+++ Javascript/box/box.js
@@ -6,8 +6,8 @@ var color = "#e6ccff";
 var g = 0.0; // acceleration due to gravity
 var x = 50;  // initial horizontal position
 var y = 50;  // initial vertical position
-var vx = 2;  // initial horizontal speed
-var vy = 0;  // initial vertical speed
+var vx = Math.random();  // initial horizontal speed
+var vy = Math.random();  // initial vertical speed
 
 window.onload = init;
 
