0d4202223c41e42e409568f59fcf3b5b9967102b
Mauricio Sevilla <jmsevillam@unal.edu.co>
2020-04-20 16:15:50 +0200

    Test_Colors

--- Javascript/Clifford/Clifford.js
+++ Javascript/Clifford/Clifford.js
@@ -25,11 +25,11 @@ function animate(){
     //canvas fill style
     $.fillStyle = "rgb(255,255,255);";
     $.fillRect (0, 0, 600, 600);
-    
+
     reset = false;
   }
   //fill drawing with random / rainbow colors
-  $.fillStyle = rndColor();
+  $.fillStyle = "rgb(0,0,0);";//rndColor();
   //draw 100 times over these x,y positions
   for(var i = 0; i < 100; i++){
     $.fillRect (300+x*100, 300+y*100, 1, 1);
